dde7e6484f62bbc0c578e0b226d52777720ff061
RAMISH104 <131403671+2vdt@users.noreply.github.com>
2026-01-14 18:55:14 +0500

    Add files via upload

---
+++ TextChanger.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from "react";
+const TextChange = () => {
+  const texts = ["Hi, I'm Rimsha", "A web developer", "Creating dynamic web experiences"];
+  const [currentText, setCurrentText] = useState("");
+  const [endValue, setendValue] = useState(0);
+  const [isForward, setIsForward] = useState(true);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentText(texts[index].substring(0, endValue));
+      if (isForward) {
+        setendValue((prev) => prev + 1);
+      } else {
+        setendValue((prev) => prev - 1);
+      }
+      if (endValue > texts[index].length + 30) {
+        setIsForward(false);
+      }
+      if (endValue < 2.1) {
+        setIsForward(true);
+        setIndex((prev) => (prev + 1) % texts.length);
+      }
+    }, 40);
+
+    return () => clearInterval(intervalId);
+  }, [endValue, isForward, index, texts]);
+
+  return <div className="transition ease duration-300">{currentText}</div>;
+};
+
+export default TextChange;
